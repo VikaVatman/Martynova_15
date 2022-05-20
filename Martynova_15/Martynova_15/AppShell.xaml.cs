c7785070e6c11c168dea60ba23e962656475f94d
Vika Vatman <vika180303@gmail.com>
2022-05-20 11:37:54 +0300

    Добавьте файлы проекта.

---
+++ Martynova_15/Martynova_15/AppShell.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Martynova_15.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Martynova_15
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ Martynova_15/Martynova_15/Views/HystoryPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Martynova_15.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HystoryPage : ContentPage
+    {
+        public HystoryPage()
+        {
+            InitializeComponent();
+
+        }
+
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            // Retrieve all the notes from the database, and set them as the
+            // data source for the CollectionView.
+            collectionView.ItemsSource = await App.DataBase.GetTimesAsync();
+        }
+    }
+}
